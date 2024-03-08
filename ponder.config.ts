0a89f7fbcd261d3408e4206628c5556c3f4c69d5
Haruxe <haruxe@proton.me>
2024-03-08 06:35:37 -0800

    ???

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,25 @@
 import { createConfig } from "@ponder/core";
-import { Abi, http } from "viem";
+import { Abi, http, webSocket } from "viem";
 
 import { abi } from "./abis/Staking";
 
+const RPC_URL = process.env.PONDER_RPC_URL_1 || "";
+
 export default createConfig({
   networks: {
-    blast: {
+    Blast: {
       chainId: 81457,
-      transport: http(process.env.PONDER_RPC_URL_1),
+      transport: webSocket(
+        "wss://rpc.ankr.com/blast/ws/e48a473be7cfb8adb30211a6b3fe9635d422bda9d4805df358f68e0959b401ef"
+      ),
     },
   },
   contracts: {
     Staking: {
-      network: "blast",
-      abi: abi as Abi,
+      network: "Blast",
+      abi: abi,
       address: "0x3d2F0400BB52798cF016406872b1933eD5d3a90e",
-      startBlock: 508777,
+      startBlock: 462863,
     },
   },
 });
